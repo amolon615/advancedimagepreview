69277740e01a3d22abee5ebbf9359bea7d5e0720
Artem Horovenko <amolon210@icloud.com>
2025-03-15 21:43:44 +0000

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "AdvancedImagePreview",
+    platforms: [
+        .iOS(.v17)
+       ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,10 +15,7 @@ let package = Package(
             targets: ["AdvancedImagePreview"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "AdvancedImagePreview"),
-
     ]
 )
