6b10954b036ca5aefb140217be669556dbbcd876
Artem Horovenko <amolon210@icloud.com>
2025-03-23 12:32:21 +0000

    Refactoring

---
+++ Sources/AdvancedImagePreview/Models/Mocks/MockImageModel.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  AdvancedImagePreview
+//
+//  Created by amolonus on 26/03/2025.
+//
+
+import Foundation
+
+struct MockImageModel {
+    var url: URL
+}
+
+extension MockImageModel {
+    static let landscapeImage: URL = URL(string: "")!
+    static let portraitImage: URL = URL(string: "")!
+}
